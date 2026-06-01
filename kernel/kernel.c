67e9e2b2c3bcb1de1c620adea1102aeea70f338b
xqill275 <oliverlong275@gmail.com>
2026-06-01 01:51:58 +0000

    fixed backspace

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -156,6 +156,17 @@ void terminal_putchar(char c)
         }
         return;
     }
+    if (c == '\b') {
+        if (terminal_column > 0) {
+            terminal_column--;
+        } else if (terminal_row > 0) {
+            terminal_row--;
+            terminal_column = VGA_WIDTH - 1;
+        }
+        terminal_putentryat(' ', terminal_color, terminal_column, terminal_row);
+        terminal_update_cursor();
+        return;
+    }
 
     terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
     if (++terminal_column == VGA_WIDTH) {
